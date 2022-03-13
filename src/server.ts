1ddb90baca65562be79b168b0a95bac8aa631800
GuhSntLean <guleandro.66@gmail.com>
2022-03-13 12:37:41 -0300

    create server archive

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+const express = require("express");
+const ip = require("ip");
+// const express = require('express')
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${ip.address()}:${port}`);
+});
